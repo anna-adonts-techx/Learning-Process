d8e69c24897b7bceaed41a376ae92b301657a5a6
anna-adonts-techx <anna.adonts.techx@gmail.com>
2025-10-03 13:58:14 +0400

    Add Temperature-Conversion

---
+++ Temperature-Conversion/index.js
@@ -0,0 +1,25 @@
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+
+let temp;
+
+function convert(e){
+    if(toFahrenheit.checked) {
+        temp = Number(textBox.value);
+        temp = (temp * (9/5)+32)
+        result.textContent = temp.toFixed(1) + 'F'
+        // toFixed(1) - 1 digital pass the decimal place
+    }
+
+    else if (toCelsius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9)
+        result.textContent = temp.toFixed(1) + 'C'
+    }
+
+    else {
+        result.textContent = "Select a unit";
+    }
+}
